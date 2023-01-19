6429f5e15e2755620cb2870f2f5a7f75d7e69a9b
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-01-19 13:44:34 +0000

    Update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,9 +6,9 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    @boook.user_id = current_user.id#ﾌｫｰﾑに入力済ﾃﾞｰﾀのﾕｰｻﾞidにﾛｸﾞｲﾝ中のﾕｰｻﾞｰid代入
+    @book.user_id = current_user.id#ﾌｫｰﾑに入力済ﾃﾞｰﾀのﾕｰｻﾞidにﾛｸﾞｲﾝ中のﾕｰｻﾞｰid代入
     if @book.save                 #ﾃﾞｰﾀ保存されたら
-       @book.user_id = current_user.id#詳細ﾍﾟｰｼﾞへ飛ぶ
+      redirect_to books_show_path#詳細ﾍﾟｰｼﾞへ飛ぶ
     else                        #ﾃﾞｰﾀ保存されなかったら
       render :index#indexﾍﾟｰｼﾞを再表示
     end
