fd1fdc5decf627ad9c943b2f12e7ad501b17694d
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-01-19 11:48:40 +0000

    Add ﾊﾞﾘﾃﾞｰｼｮﾝ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,12 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to books_show_path#詳細ﾍﾟｰｼﾞへ飛ぶ
+    @book = Book.new(book_params)
+    if @book.save                 #ﾃﾞｰﾀ保存されたら
+      redirect_to books_show_path#詳細ﾍﾟｰｼﾞへ飛ぶ
+    else                        #ﾃﾞｰﾀ保存されなかったら
+      render :index#indexﾍﾟｰｼﾞを再表示
+    end
   end
 
   def show
