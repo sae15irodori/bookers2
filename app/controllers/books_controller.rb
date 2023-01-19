17dfbcf68ed758c041b9a21fa5a788932486fef6
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-01-19 09:15:44 +0000

    Update createｱｸｼｮﾝ(詳細ﾍﾟｰｼﾞへのﾘﾀﾞｲﾚｸﾄ)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,11 +2,11 @@ class BooksController < ApplicationController
   def index
     @book = Book.new#入力ﾌｫｰﾑ
   end
-  
+
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to books_path#詳細ﾍﾟｰｼﾞへ飛ぶ
+    redirect_to books_show_path#詳細ﾍﾟｰｼﾞへ飛ぶ
   end
 
   def show
