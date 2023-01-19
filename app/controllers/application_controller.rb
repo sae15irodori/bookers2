ff15519d195ca99e2e2a1a2cbdd03564a3c9568f
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-01-19 05:14:50 +0000

    Add ﾛｸﾞｲﾝ済の場合のﾍｯﾀﾞｰ(途中)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  #ユーザー認証・登録する前に(devise利用する前)↓のメソッド実行される
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])#ﾃﾞﾌｫﾙﾄではname保存できないから保存できるようにした
+  end
+  end
 end
