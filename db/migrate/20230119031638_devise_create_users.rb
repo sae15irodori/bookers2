f78a687b13b8ad64882284c007fe9b9b126c489d
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-01-19 04:13:27 +0000

    Userﾃｰﾌﾞﾙにｶﾗﾑ追加(name,introduction)

--- db/migrate/20230119031638_devise_create_users.rb
+++ db/migrate/20230119031638_devise_create_users.rb
@@ -31,7 +31,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
-
+      t.string :name#追加した名前のカラム
+      t.string :introduction#追加した自己紹介文のカラム
 
       t.timestamps null: false
     end
