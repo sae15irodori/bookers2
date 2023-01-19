2ec5e7413224a797cbbd6aa1e9648bc995bdebb1
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-01-19 06:19:17 +0000

    nameとパスワードでログイン可能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])#ﾃﾞﾌｫﾙﾄではname保存できないから保存できるようにした
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])#
   end
   end
 end
