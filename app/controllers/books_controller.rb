8e5fd6aa6909871003eda8e20e14b6c983da1db3
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-01-19 12:01:42 +0000

    Add 部分ﾃﾝﾌﾟﾚｰﾄ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,13 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
     @book = Book.new#入力ﾌｫｰﾑ
   end
 
   def create
     @book = Book.new(book_params)
     if @book.save                 #ﾃﾞｰﾀ保存されたら
-      redirect_to books_show_path#詳細ﾍﾟｰｼﾞへ飛ぶ
+      redirect_to books_show_path(@book.id)#詳細ﾍﾟｰｼﾞへ飛ぶ
     else                        #ﾃﾞｰﾀ保存されなかったら
       render :index#indexﾍﾟｰｼﾞを再表示
     end
