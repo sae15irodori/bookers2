3fe45327dc27d4d3d57bd95a8a214c9f0e95bc4c
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-01-19 08:00:31 +0000

    Update booksｺﾝﾄﾛｰﾗ

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/show'
   devise_for :users
   root to: "homes#top"
   get 'homes/about' => 'homes#about', as: 'about'
-  resources :books, only: [:new, :index, :show]
+  resources :books, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
