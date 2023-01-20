9fe9a4c5cf57cdf4e2a557e7132bf38d2d8f4495
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-01-20 06:32:14 +0000

    Add editﾋﾞｭｰﾙｰﾃｨﾝｸﾞ/Updte showﾋﾞｭｰのeditﾘﾝｸ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,16 +17,17 @@ class BooksController < ApplicationController
   def show
     @book= Book.find(params[:id])#特定の投稿ﾃﾞｰﾀのid取得
   end
-  
+
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path
   end
-  
+
   def edit
     @book = Book.find(params[:id])
-  
+  end
+
   private
     def book_params
       params.require(:book).permit(:title, :body)
