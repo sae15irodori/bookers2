46493c055d44f9bed68e9ba20aced71d9293ee73
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-01-20 00:53:23 +0000

    Update ﾙｰﾃｨﾝｸﾞとｻｲﾝｲﾝ･ｱｳﾄ後の遷移先

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,15 @@ class ApplicationController < ActionController::Base
   class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   #ユーザー認証・登録する前に(devise利用する前)↓のメソッド実行される
+  
+    def after_sign_in_path_for(resource)
+      about_path#ここは後程変更
+    end
+    
+    
+    def after_sign_aout_path_for(resource)
+      about_path#ここは後程変更
+    end
   protected
 
   def configure_permitted_parameters
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
   devise_for :users
   root to: "homes#top"
   get 'homes/about' => 'homes#about', as: 'about'
