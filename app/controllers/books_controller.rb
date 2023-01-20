09f81d3c7e1fdc4f4f415974df39fc0c36f62568
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-01-20 05:59:15 +0000

    Add showﾍﾟｰｼﾞ内にeditへのﾘﾝｸとdestroyアクション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,8 +15,15 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book= Book.find(params[:id])#特定の投稿ﾃﾞｰﾀのid取得
   end
-
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+  
   private
     def book_params
       params.require(:book).permit(:title, :body)
